d3b34a13f6584adbbec5492793f20e0f9697de00
Ryan Reis <ryanreis280903@gmail.com>
2023-04-25 13:21:48 -0300

    crud

---
+++ AppMvcBasica/Models/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace AppMvcBasica.Models
+{
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+
+        public Guid Id { get; set; }
+    }
+
+}
---
+++ AppMvcBasica/Models/TipoFornecedor.cs
@@ -0,0 +1,9 @@
+﻿namespace AppMvcBasica.Models
+{  
+    
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PerssoaJuridica = 2
+    }
+}
